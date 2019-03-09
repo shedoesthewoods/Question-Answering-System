3e63b64d4affb4b3157a1c04660cb6db1b50582a
shedoesthewoods <mmelike.mat@gmail.com>
2019-03-09 14:43:20 +0300

    yorum satırları eklendi

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,36 @@
 package chatbot;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
+    // input olarak girilebilecek keyword için açılmış arraylist
     public static ArrayList<String> keywords = new ArrayList<>();
 
     public static void main(String[] args) {
+        // keywords için eleman eklemeleri
         keywords.add("kaç");
 
-        // TODO Auto-generated method stub
-        new Bot();
+        // Bot sınıfında yazılan metodlar bot değişkeni üzerinden çalıştırılacak
+        Bot bot = new Bot();
+
+        // konsoldan okumalar
+        Scanner input = new Scanner(System.in);
+        System.out.print("Sormak istediğiniz soru nedir?(Çıkış için 0 girin): ");
+        String girdi = input.nextLine(); //nextLine yerine next metodu kullanıldığında uygulama bozuluyor
+
+        // arayüz için yazılmış bir satır
+        System.out.println("You: " + girdi);
+
+        //girilen cümleyi kelime kelime ayırıyor
+        ArrayList<String> words = bot.parse(girdi);
+
+        //kullanıcı çıkış yapmak isteyene kadar uygulamayı açık tutan döngü
+        while(girdi.compareTo("0")!=0) {
+            //uygulamanın verdiği cevaplar answer metodunda ayarlanacak
+            bot.botSay(bot.answer(words));
+            System.out.print("Sormak istediğiniz soru nedir?(Çıkış için 0 girin): ");
+            girdi = input.nextLine();
+        }
     }
 }
