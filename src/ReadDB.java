bd27cb6aaebd60296a68a2827c512aba331f2ded
shedoesthewoods <mmelike.mat@gmail.com>
2019-03-20 02:04:56 +0300

    son hal

--- src/ReadDB.java
+++ src/ReadDB.java
@@ -2,24 +2,24 @@ package chatbot;
 
 import java.sql.*;
 
-class ReadDB {
+public class ReadDB {
 	//JDBC driver adi and veritabani URL
-	static final String JDBC_DRIVER = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
-	static final String DB_URL = "jdbc:sqlserver://LAPTOP-RKHJINC7:1433;DatabaseName=QuestionAnswering";
+	private static final String JDBC_DRIVER = "com.mysql.sqlserver.jdbc.Driver";
+	private static final String DB_URL = "jdbc:mysql://LAPTOP-RKHJINC7:1433;DatabaseName=QuestionAnswering";
 
 	//Veritabani baglantisi
-	static final String USER = "sa";
-	static final String PASS = "limonata";
+	private static final String USER = "sa";
+	private static final String PASS = "limonata";
 
-	Connection conn = null;
-	Statement stmt = null;
+	private Connection conn = null;
+	private Statement stmt = null;
 
 	ReadDB(){ }
 
-	void read() {
+	void read(String sql) {
 		try{
 			//JDBC driver
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			Class.forName(JDBC_DRIVER);
 
 			//Baglanti acma
 			conn = DriverManager.getConnection(DB_URL, USER, PASS);
@@ -27,41 +27,43 @@ class ReadDB {
 			//Sorgu calistirma
 			stmt = conn.createStatement();
 
-			String sql = "Select Id, Name,Lastname,Hand,Birthday,Country From atp_players;";
+			//String sql = "Select Id, Name,Lastname,Hand,Birthday,Country From atp_players;";
 			ResultSet rs = stmt.executeQuery(sql);
+
 			//ResultSet'ten verileri cekme
 			while(rs.next()){
 				//Sutun adlarini alma
 				int id  = rs.getInt("Id");
 				String name = rs.getString("Name");
 				String lastname = rs.getString("Lastname");
-				String birthday = String.valueOf(rs.getInt("Birthday")); //date tipine setbDate metoduyla cast ediliyor
+				String birthday = rs.getString("Birthday");
 				String hand = rs.getString("Hand");
 				String country = rs.getString("Country");
+				String cinsiyet = rs.getString("Cinsiyet");
 
-				System.out.println("ID: " + id + ", Name: " + name + ", Lastname: "+ lastname + ", Hand: "+ hand+
-						", Birthday: " + birthday + ", Country: " + country);
+				System.out.println("ID: " + id + ", Name: " + name + ", Lastname: "+ lastname +
+						", Hand: "+ hand+ ", Birthday: " + birthday + ", Country: " +
+							country + ", Cinsiyet: "+cinsiyet);
 			}
-			rs.close();
-		}catch(SQLException se){
-			//JDBC icin hata ayiklama
-			se.printStackTrace();
-		}catch(Exception e){
-			//Class.forName icin hata ayiklama
-			e.printStackTrace();
-		}finally{
-			try{
-				if(stmt!=null)
-					conn.close();
+				rs.close();
 			}catch(SQLException se){
+				//JDBC icin hata ayiklama
 				se.printStackTrace();
+			}catch(Exception e){
+				//Class.forName icin hata ayiklama
+				e.printStackTrace();
+			}finally{
+				try{
+					if(stmt!=null) conn.close();
+				}catch(SQLException se){
+					se.printStackTrace();
+				}
+
+				try{
+					if(conn!=null) conn.close();
+				}catch(SQLException se){
+					se.printStackTrace();
+				}
 			}
-			try{
-				if(conn!=null)
-					conn.close();
-			}catch(SQLException se){
-				se.printStackTrace();
-			}
-		}
 	}
 }
